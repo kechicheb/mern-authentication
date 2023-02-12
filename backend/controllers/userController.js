339d68bb1bb2cf564e358829a72cfd35eb2c2848
Ahmed Kechicheb <ahmedkechicheb13@gmail.com>
2023-02-12 14:59:46 +0100

    validate email & password before signup

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,20 +1,20 @@
-const User = require('../models/userModel')
+const User = require("../models/userModel");
 
 // login a user
 const loginUser = async (req, res) => {
-  res.json({mssg: 'login user'})
-}
+  res.json({ mssg: "login user" });
+};
 
 // signup a user
 const signupUser = async (req, res) => {
-    const {email, password} = req.body
-  
-    try {
-      const user = await User.signup(email, password)
-  
-      res.status(200).json({email, user})
-    } catch (error) {
-      res.status(400).json({error: error.message})
-    }
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.signup(email, password);
+
+    res.status(200).json({ email, user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
   }
-module.exports = { signupUser, loginUser }
+};
+module.exports = { signupUser, loginUser };
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
-const userRoutes = require('./routes/user')
+const userRoutes = require("./routes/user");
 // express app
 const app = express();
 
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 
 // routes
 app.use("/api/workouts", workoutRoutes);
-app.use('/api/user', userRoutes)
+app.use("/api/user", userRoutes);
 
 // connect to db
 mongoose.set("strictQuery", false);
