c94698d0507a60b0bedc7a4ee49422b12d7440ef
Ahmed Kechicheb <ahmedkechicheb13@gmail.com>
2023-02-12 16:46:02 +0100

    added Login & Signup Forms styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,10 @@ import { Routes, Route } from "react-router-dom";
 
 // pages & components
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div className="App">
@@ -10,6 +13,8 @@ function App() {
       <div className="pages">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </div>
     </div>
