e0a197e8ca7b0ffa908ce05cabedeb0717bd8e09
Ahmed Kechicheb <ahmedkechicheb13@gmail.com>
2023-02-13 13:03:15 +0100

    useLogout hook in Navbar components

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,10 @@
-import { Link } from 'react-router-dom'
-
+import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
 const Navbar = () => {
+  const { logout } = useLogout();
+  const handleClick = () => {
+    logout();
+  };
 
   return (
     <header>
@@ -10,13 +14,16 @@ const Navbar = () => {
         </Link>
         <nav>
           <div>
+            <button onClick={handleClick}>Log out</button>
+          </div>
+          <div>
             <Link to="/login">Login</Link>
             <Link to="/signup">Signup</Link>
           </div>
         </nav>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
